e5c6764c8ade5d20ad68a8fab90078e9a9d5e49f
Aymeric Bouzigues <a.bouzigues@outlook.com>
2019-04-17 17:54:01 +0200

    Handled bear game features

--- src/index.js
+++ src/index.js
@@ -2,27 +2,86 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
+  return null;
+}
+
+function Symbol(props) {
+  if (props.value === 'X') {
+    return <i className="fas fa-times"> </i>
+  }
+  else if (props.value === 'O') {
+    return <i className="far fa-circle"> </i>
+  }
+  return null
+}
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      <Symbol value={props.value}></Symbol>
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  state = {
+    squares: Array(9).fill(null),
+    xIsNext: true,
+  };
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+      />;
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status =
+      <div className="status">
+        <Symbol value={winner}> </Symbol> won !
+      </div>;
+    } else {
+      status =
+      <div className="status">
+        {"It's "}<Symbol value={this.state.xIsNext ? 'X' : 'O'}> </Symbol>{"'s turn to play"}
+      </div>;
+    }
     return (
       <div>
-        <div className="status">{status}</div>
+        {status}
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
